b9fb6455020fffc2117369afae68e59e3870a776
Egzon Mustafa <egzonmustafa07@gmail.com>
2018-02-24 14:15:10 +0100

    Database Commit

---
+++ AirlineDatabase/Security/usrAirline.sql
@@ -0,0 +1,2 @@
+﻿CREATE USER [usrAirline] FOR LOGIN [usrAirline];
+
---
+++ AirlineDatabase/Security/usrAirline_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [usrAirline]
+    WITH PASSWORD = N'{ZEdee%|wglsmsQs`t>woqldmsFT7_&#$!~<na{smSRhkjpm', SID = 0xBACF5CE58F4C9146BF5832E1CA03F04F, DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+
